788b307b5df26edf6cb36306cfe36e481c2e1378
Andrey Kim <akart.dev@gmail.com>
2021-08-07 04:00:34 +0600

    Add UIAlertController

--- WeatherApp/Controllers/ViewController.swift
+++ WeatherApp/Controllers/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var weatherIconImageView: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var feelsLikeTemperatureLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+        
+    }
+    
 
 }
 
--- WeatherApp/Extension.swift
+++ WeatherApp/Extension.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?,
+                                      message: String?,
+                                      style: UIAlertController.Style) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: style)
+        ac.addTextField { tf in
+            let cities = ["Almaty", "Moscow", "Seoul", "New York"]
+            tf.placeholder = cities.randomElement()
+        }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = ac.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        ac.addAction(search)
+        ac.addAction(cancel)
+        present(ac, animated: true, completion: nil)
+    }
+}
