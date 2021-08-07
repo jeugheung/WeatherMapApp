b65dcbdf7476afac09a766843f7822d9781be10b
Andrey Kim <akart.dev@gmail.com>
2021-08-08 03:37:14 +0600

    Alert changes

--- WeatherApp/Extension.swift
+++ WeatherApp/Extension.swift
@@ -11,7 +11,8 @@ import UIKit
 extension ViewController {
     func presentSearchAlertController(withTitle title: String?,
                                       message: String?,
-                                      style: UIAlertController.Style) {
+                                      style: UIAlertController.Style,
+                                      completionHandler: @escaping(String) -> Void) {
         let ac = UIAlertController(title: title, message: message, preferredStyle: style)
         ac.addTextField { tf in
             let cities = ["Almaty", "Moscow", "Seoul", "New York"]
@@ -21,7 +22,9 @@ extension ViewController {
             let textField = ac.textFields?.first
             guard let cityName = textField?.text else { return }
             if cityName != "" {
+                let city = cityName.split(separator: " ").joined(separator: "%20")
                 print("search for the \(cityName)")
+                completionHandler(city)
             }
         }
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
